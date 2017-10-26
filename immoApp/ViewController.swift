d6dfa09e054466672c13835b871eac375a090376
Joas LOZ <joas_loz@Joass-Mac.local>
2017-10-26 16:53:03 -0400

    Initial Commit

---
+++ immoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  immoApp
+//
+//  Created by Joas LOZ on 26/10/2017.
+//  Copyright © 2017 Joas LOZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
